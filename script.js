4387629674112bbca6adeb9c0b0ce260af1e6970
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-14 10:58:42 -0400

    Complete theory part

---
+++ script.js
@@ -0,0 +1,54 @@
+// function hello() {
+//   alert('HEY!');
+// }
+
+// setInterval(hello, 6000);
+
+// function hello() {
+//   alert('How are you doing?');
+// }
+
+// setInterval(hello, 3000);
+
+
+// const colorArray = ['#9A208C', '#FFEAEA', '#E11299', '#F5C6EC', '#D61355', '#FF8E9E', '#EFA3C8'];
+// setInterval(colorChange, 1000);
+
+// let i = 0;
+
+// function colorChange() {
+//   document.body.style.backgroundColor = colorArray[i];
+//   i++;
+  
+//   if (i > colorArray.length) {
+//     i = 0;
+//   }
+// }
+
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10, 22, 23,24,27];
+
+// function evenNum(array) {
+// return array.filter(element => element % 2 === 0);
+// }
+
+// alert(evenNum(numbers));
+
+// const par = document.querySelector('#par');
+// const button = document.querySelector('#submit');
+
+// button.addEventListener('click', getRandomNumber);
+
+// function getRandomNumber() {
+//   const randomNumber = Math.floor(Math.random() * 101);
+//   document.querySelector('#par').textContent = randomNumber;
+//   // par.textContent = randomNumber;
+// }
+
+// const now = new Date();
+// console.log(now);
+
+// const myDate = new Date();
+// const month = myDate.getMonth();
+// const day = myDate.getDate();
+
